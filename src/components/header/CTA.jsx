019a4af11456c5393e2702db7565257d48b2d851
mostafa-aboelaela <aboelaela20@gmail.com>
2023-03-03 20:33:01 +0200

    changes

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import CV from '../../assets/Mostafa-Abdalhamed-CV.pdf';
+import cv from '../../assets/Mostafa-Abdalhamed-CV.pdf';
 
 const CTA = () => {
   return (
     <div className="cta">
-      <a href={CV} download className="btn">
+      <a href={cv} download className="btn">
         Download CV
       </a>
       <a href="#contact" className="btn btn-primary">
